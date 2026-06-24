4b68d2890e6dd0646e3cb767b0393f67a06b7cba
Kpytoo <coddaniel46@gmail.com>
2026-06-24 11:56:17 -0400

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
 	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
 	sf::CircleShape shape( 100.f );
 	shape.setFillColor( sf::Color::Green );
+
+	sf::Angle angle1 = sf::degrees(90);
+	std::cout << angle1.asDegrees() << "\n";
 
 	while ( window.isOpen() )
 	{
